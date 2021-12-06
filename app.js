ea281f919a0283370c386b163d1c7ca14d3555a0
ducblack <tongminhduc237@gmail.com>
2021-12-06 22:48:04 +0700

    chuyen huong request

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const http = require('http');
-const { text } = require('stream/consumers');
+
 const server = http.createServer((req,res)=>{
     const url = req.url;
     if(url==='/'){
@@ -8,6 +8,7 @@ const server = http.createServer((req,res)=>{
         res.write('<body><form action ="/message" method="post"><input type="text" name="message"><button type="submit">Send</button></form></body>');
         res.write('</html>');
         return res.end(); 
+        
     }
     res.setHeader('Content-Type','text/html');
     res.write('<html>');
