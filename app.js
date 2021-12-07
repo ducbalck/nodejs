7717d4d31a33208c9efa7046f57b88e1d0445f9f
ducblack <tongminhduc237@gmail.com>
2021-12-07 23:41:53 +0700

    phan tich cu phap request den

--- app.js
+++ app.js
@@ -1,19 +1,19 @@
 const express = require('express');
-
+const bodyParser = require('body-parser');
 const app= express();
 
-app.use('/',(req,res,next)=>{
-    console.log('This always run !');
-    next();
-});
+app.use(bodyParser.urlencoded({extended:false}));
 
 app.use('/app-product',(req,res,next)=>{
-    console.log('In another middlewase');
-    res.send('<h1>The "Add Product"</h1>');    
+    res.send('<form action ="/product" method="post"><input type="text" name="title"><button type="submit">Add Product</button></form>');    
+});
+
+app.use('/product',(req,res,next)=>{
+    console.log(req.body);
+    res.redirect('/');  
 });
 
 app.use('/',(req,res,next)=>{
-    console.log('In another middlewase');
     res.send('<h1>Hello from Express</h1>');    
 });
 
