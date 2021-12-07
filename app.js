40366a75aecb5714edb10b0ad96ad6120ab61d28
ducblack <tongminhduc237@gmail.com>
2021-12-07 22:27:29 +0700

    cai dat middleware

--- app.js
+++ app.js
@@ -4,6 +4,17 @@ const express = require('express');
 
 const app= express();
 
+app.use((req,res,next)=>{
+    console.log('In the middlewase');
+    next();
+});
+
+app.use((req,res,next)=>{
+    console.log('In another middlewase');
+    
+});
+
+
 const server = http.createServer(app);
 
 server.listen(3000);
