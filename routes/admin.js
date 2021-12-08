abaf0e502d733a1a0e5eec7ec89cf031ed24fd43
ducblack <tongminhduc237@gmail.com>
2021-12-08 22:43:28 +0700

    chia se du lieu tu cac req , user

--- routes/admin.js
+++ routes/admin.js
@@ -2,13 +2,15 @@ const path= require('path');
 const rooDir=require('../util/path');
 const express = require('express');
 const router = express.Router();
+const products=[];
 
 router.get('/add-product',(req,res,next)=>{
     res.sendFile(path.join(rooDir,'views','add-product.html'));    
 });
 
 router.post('/add-product',(req,res,next)=>{
-    console.log(req.body);
+    products.push({title:req.body.title});
     res.redirect('/');  
 });
-module.exports= router;
+exports.routes =router;
+exports.products =products
--- routes/shop.js
+++ routes/shop.js
@@ -2,8 +2,10 @@ const path= require('path');
 const rooDir=require('../util/path');
 const express = require('express');
 const router = express.Router();
+const adminData=require('./admin');
 
 router.get('/',(req,res,next)=>{
+    console.log('shop.js',adminData.products);
     res.sendFile(path.join(rooDir,'views','shop.html'));    
 });
 module.exports= router;
