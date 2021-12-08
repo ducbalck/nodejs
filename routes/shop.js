223e754db3a6ab2f18bc23fbbad98203696291ed
ducblack <tongminhduc237@gmail.com>
2021-12-09 01:47:46 +0700

    hoan thien pug

--- routes/shop.js
+++ routes/shop.js
@@ -6,6 +6,6 @@ const adminData=require('./admin');
 
 router.get('/',(req,res,next)=>{
     const products= adminData.products;
-    res.render('shop',{prods:products, docTitle:'shop'}) ;
+    res.render('shop',{prods:products, pageTitle:'shop',path:'/'}) ;
 });
 module.exports= router;
