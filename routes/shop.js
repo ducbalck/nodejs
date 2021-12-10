28b4e69357e251f6ba38ef66b6025454bdd53f23
ducblack <tongminhduc237@gmail.com>
2021-12-11 00:42:06 +0700

    them id san pham vao duong dan

--- routes/shop.js
+++ routes/shop.js
@@ -1,7 +1,19 @@
-const path= require('path');
-const express = require('express');
-const router = express.Router();
-const productsControllers = require('../controllers/product');
+const path = require('path');
 
-router.get('/',productsControllers.getProducts);
-module.exports= router;
+const express = require('express');
+
+const shopController = require('../controllers/shop');
+
+const router = express.Router();
+
+router.get('/', shopController.getIndex);
+
+router.get('/products', shopController.getProducts);
+
+router.get('/cart', shopController.getCart);
+
+router.get('/orders', shopController.getOrders);
+
+router.get('/checkout', shopController.getCheckout);
+
+module.exports = router;
