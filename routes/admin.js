bcd85849da52be151ad8342fa3421791977003a6
ducblack <tongminhduc237@gmail.com>
2021-12-10 22:16:58 +0700

    sualoi

--- routes/admin.js
+++ routes/admin.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 
-router.get('/app-product',(req,res,next)=>{
+router.get('/add-product',(req,res,next)=>{
     res.send('<form action ="/admin/add-product" method="post"><input type="text" name="title"><button type="submit">Add Product</button></form>');    
 });
 
