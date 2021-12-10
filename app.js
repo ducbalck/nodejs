9e217c69e5a7e1b082c8d163b6ad6dd1b519031e
ducbalck <92384023+ducbalck@users.noreply.github.com>
2021-12-10 20:53:12 +0700

    sua loi

--- app.js
+++ app.js
@@ -18,8 +18,8 @@ app.use('/admin',adminRoutes);
 
 app.use(shopRouter);
 
-app.use((req,res,next)=>{
-    res.status(404).render('404',{pageTitle:'Page Not Found'});
-});
+app.use((req, res, next) => {
+    res.status(404).render('404', { pageTitle: 'Page Not Found', path: '/404' });
+    });
 
 app.listen(3000);
