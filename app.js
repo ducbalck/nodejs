e9ff7d662fadcffa72e98ac28ee4ea8d33bfbf76
ducblack <tongminhduc237@gmail.com>
2021-12-10 20:51:43 +0700

    sua loi

--- app.js
+++ app.js
@@ -1,25 +1,24 @@
-const path= require('path');
-const express= require('express');
-const bodyParser = require('body-parser');
-const expressHbs = require('express-handlebars');
+const path = require('path');
 
-const app= express();
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
 
 app.set('view engine', 'ejs');
-app.set('views','views');
+app.set('views', 'views');
 
-const adminData= require('./routes/admin');
-const shopRouter= require('./routes/shop');
+const adminData = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
 
-app.use(bodyParser.urlencoded({extended:false}));
-app.use(express.static(path.join(__dirname,'public')));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/admin',adminData.routes);
+app.use('/admin', adminData.routes);
+app.use(shopRoutes);
 
-app.use(shopRouter);
-
-app.use((req,res,next)=>{
-    res.status(404).render('404',{pageTitle:'Page Not Found'});
-});
+app.use((req, res, next) => {
+    res.status(404).render('404', { pageTitle: 'Page Not Found', path: '/404' });
+    });
 
 app.listen(3000);
