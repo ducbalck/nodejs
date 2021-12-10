5bb9aeb99607227e6fe1266e7c0b30223f734482
ducblack <tongminhduc237@gmail.com>
2021-12-10 22:18:57 +0700

    sua

--- routes/admin.js
+++ routes/admin.js
@@ -2,7 +2,7 @@ const path= require('path');
 const express = require('express');
 const router = express.Router();
 
-router.get('/app-product',(req,res,next)=>{
+router.get('/add-product',(req,res,next)=>{
     res.sendFile(path.join(__dirname,'../','views','add-product.html'));    
 });
 
