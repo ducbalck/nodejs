8b041541f4d660e3132d9386d42514361f87fc52
ducblack <tongminhduc237@gmail.com>
2021-12-10 23:39:34 +0700

    sualoi

--- models/product.js
+++ models/product.js
@@ -1,29 +1,22 @@
 const fs = require('fs');
 const path = require('path');
 
-const p = path.join(
-  path.dirname(process.mainModule.filename),
-  'data',
-  'products.json'
-);
-
-const getProductsFromFile = cb => {
-  fs.readFile(p, (err, fileContent) => {
-    if (err) {
-      cb([]);
-    } else {
-      cb(JSON.parse(fileContent));
-    }
-  });
-};
-
 module.exports = class Product {
   constructor(t) {
     this.title = t;
   }
 
   save() {
-    getProductsFromFile(products => {
+    const p = path.join(
+      path.dirname(process.mainModule.filename),
+      'data',
+      'products.json'
+    );
+    fs.readFile(p, (err, fileContent) => {
+      let products = [];
+      if (!err) {
+        products = JSON.parse(fileContent);
+      }
       products.push(this);
       fs.writeFile(p, JSON.stringify(products), err => {
         console.log(err);
@@ -32,6 +25,16 @@ module.exports = class Product {
   }
 
   static fetchAll(cb) {
-    getProductsFromFile(cb);
+    const p = path.join(
+      path.dirname(process.mainModule.filename),
+      'data',
+      'products.json'
+    );
+    fs.readFile(p, (err, fileContent) => {
+      if (err) {
+        cb([]);
+      }
+      cb(JSON.parse(fileContent));
+    });
   }
 };
