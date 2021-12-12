d203d36bde41a9490eeab3adc63b5cf3e25bf70f
ducblack <tongminhduc237@gmail.com>
2021-12-13 00:33:38 +0700

    sua

--- util/database.js
+++ util/database.js
@@ -1,9 +1,11 @@
+
 const mysql = require('mysql2');
 
-const pool =mysql.createPool({
-    host:'localhost',
+const pool = mysql.createPool({
+    host: 'localhost',
     user: 'root',
-    database:'node-complete',
-    password:'minhduc237'
-})
-module.exports =pool.promise();
+    database: 'node-complete',
+    password: 'mjnhduc237'
+});
+
+module.exports = pool.promise();
