c9359781ab41b587210c6cd56f2ae43bc3ce6848
ducblack <tongminhduc237@gmail.com>
2021-12-13 20:51:36 +0700

    ket noi voi csdl

--- util/database.js
+++ util/database.js
@@ -1,10 +1,8 @@
-const mysql = require('mysql2');
+const Sequelize =  require('sequelize');
 
-const pool = mysql.createPool({
-    host: 'localhost',
-    user: 'root',
-    database: 'node-complete',
-    password: 'mjnhduc237'
+const sequelize = new Sequelize('node-complete','root', 'mjnhduc237',{
+    dialect:'mysql',
+    host: 'localhost'
 });
 
-module.exports = pool.promise();
+module.exports = sequelize ;
