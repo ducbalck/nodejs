270a5156dfa9c59c7e3ac376e12a14189220d37b
ducblack <tongminhduc237@gmail.com>
2021-12-15 21:52:52 +0700

    cai dat trinh dieu khien mongodb

--- util/database.js
+++ util/database.js
@@ -1,8 +1,18 @@
-const Sequelize =  require('sequelize');
+const mongodb =  require('mongodb');
 
-const sequelize = new Sequelize('node-complete','root', 'mjnhduc237',{
-    dialect:'mysql',
-    host: 'localhost'
-});
+const MongoClient = mongodb.MongoClient;
 
-module.exports = sequelize ;
+const mongoConnect = callback =>{
+    MongoClient.connect(
+        'mongodb+srv://minhduc237:mjnhduc237@cluster0.ei7bq.mongodb.net/myFirstDatabase?retryWrites=true&w=majority')
+        .then(client =>{
+            console.log('Connected');
+            callback(client);
+        })
+        .catch(err =>{
+            console.log(err);
+        })
+    }
+        
+
+module.exports = mongoConnect ;
