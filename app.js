635a5ceafe3e2ccfb3d080913a8ffee85af02f63
ducblack <tongminhduc237@gmail.com>
2021-12-16 21:31:03 +0700

    hoàn thành code cập nhật sản phẩm

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const bodyParser = require('body-parser');
 
 const errorController = require('./controllers/error');
 const mongoConnect = require('./util/database').mongoConnect;
+
 const app = express();
 
 app.set('view engine', 'ejs');
@@ -17,7 +18,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use((req, res, next) => {
-  // User.findByPk(1)
+  // User.findById(1)
   //   .then(user => {
   //     req.user = user;
   //     next();
@@ -31,7 +32,6 @@ app.use(shopRoutes);
 
 app.use(errorController.get404);
 
-mongoConnect(()=>{
-  
+mongoConnect(() => {
   app.listen(3000);
 });
