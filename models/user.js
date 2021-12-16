43d0737a66c9e5ce6b57a6181739ae85a53554a0
ducblack <tongminhduc237@gmail.com>
2021-12-17 00:18:29 +0700

    thêm chức năng thêm vào giỏ hàng

--- models/user.js
+++ models/user.js
@@ -1,29 +1,41 @@
-const mongodb = require('mongodb');
-const getDb = require('../util/database').getDb;
+const mongodb = require("mongodb");
+const getDb = require("../util/database").getDb;
 
 const ObjectId = mongodb.ObjectId;
 
 class User {
-  constructor(username, email) {
+  constructor(username, email, cart, id) {
     this.name = username;
     this.email = email;
+    this.cart = cart; // {items: []}
+    this._id = id;
   }
 
   save() {
     const db = getDb();
-    return db.collection('users').insertOne(this);
+    return db.collection("users").insertOne(this);
+  }
+  addToCart(product) {
+    const updateCart = { items: [{ productId: new ObjectId(product._id), quantity: 1 }] };
+    const db = getDb();
+    return db
+      .collection("users")
+      .updateOne(
+        { _id: new ObjectId(this._id) },
+        { $set: { cart: updateCart } }
+      );
   }
 
   static findById(userId) {
     const db = getDb();
     return db
-      .collection('users')
+      .collection("users")
       .findOne({ _id: new ObjectId(userId) })
-      .then(user => {
+      .then((user) => {
         console.log(user);
         return user;
       })
-      .catch(err => {
+      .catch((err) => {
         console.log(err);
       });
   }
