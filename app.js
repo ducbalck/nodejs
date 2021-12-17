d66e33927f7e1dbd1758aaa39a6b0929107bde8d
ducblack <tongminhduc237@gmail.com>
2021-12-17 21:04:23 +0700

    lưu trữ dữ liệu thông qua mongoose

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 
 const errorController = require("./controllers/error");
-const User = require("./models/user");
+// const User = require("./models/user");
 
 const app = express();
 
@@ -18,15 +18,15 @@ const shopRoutes = require("./routes/shop");
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "public")));
 
-app.use((req, res, next) => {
-  User.findById("61bb58c62a032022dfbe8cd3")
-    .then((user) => {
-      req.user = new User(user.name, user.email, user.cart, user._id);
+// app.use((req, res, next) => {
+//   User.findById("61bb58c62a032022dfbe8cd3")
+//     .then((user) => {
+//       req.user = new User(user.name, user.email, user.cart, user._id);
 
-      next();
-    })
-    .catch((err) => console.log(err));
-});
+//       next();
+//     })
+//     .catch((err) => console.log(err));
+// });
 
 app.use("/admin", adminRoutes);
 app.use(shopRoutes);
@@ -35,7 +35,7 @@ app.use(errorController.get404);
 
 mongoose
   .connect(
-    "mongodb+srv://minhduc237:mjnhduc237@cluster0.ei7bq.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
+    "mongodb+srv://minhduc237:mjnhduc237@cluster0.ei7bq.mongodb.net/shop?retryWrites=true&w=majority"
   )
   .then((result) => {
     app.listen(3000);
