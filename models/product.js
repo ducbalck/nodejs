156f78a14fc9027681ea4c58a1d7a119ed372f0c
ducblack <tongminhduc237@gmail.com>
2021-12-28 20:33:33 +0700

    thêm giải pháp đăng nhập định hướng bởi request

--- models/product.js
+++ models/product.js
@@ -19,12 +19,13 @@ const productSchema = new Schema({
     type: String,
     required: true
   },
-  userId:{
-    type:Schema.Types.ObjectId,
-    ref :'User',
-    required:true
+  userId: {
+    type: Schema.Types.ObjectId,
+    ref: 'User',
+    required: true
   }
 });
+
 module.exports = mongoose.model('Product', productSchema);
 
 // const mongodb = require('mongodb');
