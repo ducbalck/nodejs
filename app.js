e42cac5d82473e031929b9f457760098c66e9f82
ducblack <tongminhduc237@gmail.com>
2021-12-28 22:45:22 +0700

    khởi tạo middleware session

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const path = require('path');
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
+const session = require('express-session');
 
 const errorController = require('./controllers/error');
 const User = require('./models/user');
@@ -17,6 +18,9 @@ const shopRoutes = require('./routes/shop');
 const authRoutes = require('./routes/auth');
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(
+  session({secret:'my secret',resave:false,saveUninitialized:false})
+);
 
 app.use((req, res, next) => {
   User.findById('61bca44c30537d702b79e031')
