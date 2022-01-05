60b212e26e94ff6544d300cfc87a2284ef3e5b1e
ducblack <tongminhduc237@gmail.com>
2022-01-05 22:19:40 +0700

    Sử dụng Middleware xử lý lỗi ở Express.js

--- app.js
+++ app.js
@@ -46,16 +46,16 @@ app.use((req, res, next) => {
     return next();
   }
   User.findById(req.session.user._id)
-  .then(user => {
-    if(!user){
-      return next();
-    }
-    req.user = user;
-    next();
-  })
-  .catch(err =>{
-    throw new Error(err);
-  });
+    .then(user => {
+      if (!user) {
+        return next();
+      }
+      req.user = user;
+      next();
+    })
+    .catch(err => {
+      throw new Error(err);
+    });
 });
 
 app.use((req, res, next) => {
@@ -71,6 +71,11 @@ app.use(authRoutes);
 app.get('/500', errorController.get500);
 
 app.use(errorController.get404);
+
+app.use((error, req, res, next) => {
+  // res.status(error.httpStatusCode).render(...);
+  res.redirect('/500');
+});
 
 mongoose
   .connect(MONGODB_URI)
